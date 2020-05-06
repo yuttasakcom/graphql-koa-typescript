396dd3aae2be79f9739556482edae6db33adc9e5
Yuttasak (Yo) Pannawat <yuttasakyo@gmail.com>
2020-05-06 21:25:33 +0700

    Init GraphQL

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as koa from 'koa'
 import { Container } from 'typedi'
+import { ApolloServer, gql } from 'apollo-server-koa'
 import {
   RoutingControllersOptions,
   useKoaServer,
@@ -15,16 +16,32 @@ export class Server {
 
   constructor() {
     this.app = new koa()
+
+    const typeDefs = gql`
+      type Query {
+        hello: String
+      }
+    `
+
+    const resolvers = {
+      Query: {
+        hello: () => 'Hello world!',
+      },
+    }
+
+    const server = new ApolloServer({ typeDefs, resolvers })
+    server.applyMiddleware({ app: this.app })
+
+    this.init()
+  }
+
+  private init(): void {
     const routingControllersOptions: RoutingControllersOptions = {
       defaultErrorHandler: false,
       controllers: [`${__dirname}/**/*.controller.ts`],
     }
-
     useKoaServer(this.app, routingControllersOptions)
-    this.init()
   }
-
-  private init(): void {}
 
   public start(): void {
     this.app.listen(this.port, this.host, () => {
